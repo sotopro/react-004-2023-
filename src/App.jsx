0551b35cd5916fac1008d5a65705abacf512af52
Daniel Soto <dasg.1993@gmail.com>
2023-06-21 09:56:43 -0500

    [feature] - create counter component and implement state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { useState } from 'react'
 import './App.css'
 import Header from './components/header'
+import Counter from './components/counter';
 
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const isValidCounter = counter > 0;
+
+  const incrementCounter = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
+  const decrementCounter = () => {
+    if(!isValidCounter) return;
+    setCounter((prevCounter) => prevCounter -1);
+  };
 
   return (
     <div>
-      <Header logo="Ds" />
+      <Header logo="Ds"/>
+      <Counter isValidCounter={isValidCounter}  counter={counter} onDecrementCounter={decrementCounter} onIncrementCounter={incrementCounter} />
     </div>
   )
 }
